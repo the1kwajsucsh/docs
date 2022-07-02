7ca572bcfd8e897a305f3b0dc785a94837e0c861
Charles Vidro <cjvidro@mtu.edu>
2022-07-02 10:34:17 -0400

    GH pages setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  assetPrefix: isProd ? '/docs/' : '',
   productionBrowserSourceMaps: true,
   images: {
     domains: ['codesandbox.io'],
@@ -13,36 +16,6 @@ module.exports = {
       {
         source: '/docs/:slug*',
         destination: '/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/jotai',
-        destination: 'https://jotai.pmnd.rs/docs/introduction',
-        permanent: true,
-      },
-      {
-        source: '/jotai/:slug*',
-        destination: 'https://jotai.pmnd.rs/docs/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/react-spring',
-        destination: 'https://react-spring.io',
-        permanent: true,
-      },
-      {
-        source: '/react-spring/:slug*',
-        destination: 'https://react-spring.io/#:slug*',
-        permanent: true,
-      },
-      {
-        source: '/drei',
-        destination: 'https://github.com/pmndrs/drei#readme',
-        permanent: true,
-      },
-      {
-        source: '/drei/:slug*',
-        destination: 'https://github.com/pmndrs/drei#:slug*',
         permanent: true,
       },
     ]
