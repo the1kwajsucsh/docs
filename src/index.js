2a76b4ebf94f72e43064cb7b747d1b7245b99e7a
The1Kwa1Jsucsh <the1kwa1jsucsh@gmail.com>
2022-07-03 22:19:51 -0400

    Updated base url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import R3fIndex from "./Components/R3F/R3fIndex";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="r3f" element={<R3fIndex />} />
